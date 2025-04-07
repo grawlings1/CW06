bacae6546db29bbb2ed6eeb774b3451e022e1071
grawlings1 <grawlings2@student.gsu.edu>
2025-04-07 12:54:23 -0400

    Create auth_service.dart

---
+++ lib/auth_service.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<User?> signUp(String email, String password) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      return result.user;
+    } catch (e) {
+      print("Sign Up Error: $e");
+      return null;
+    }
+  }
+
+  Future<User?> signIn(String email, String password) async {
+    try {
+      UserCredential result = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      return result.user;
+    } catch (e) {
+      print("Sign In Error: $e");
+      return null;
+    }
+  }
+
+  Future<void> signOut() async {
+    await _auth.signOut();
+  }
+}
